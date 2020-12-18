b64e4f3522807dd37ae466ffeb4d04ee4aa7cbb7
sahrun <sharunkk@eyerov.com>
2020-12-18 19:24:19 +0530

    resolved search issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
+import youtube from '../api/youtubeApi'
+
+const KEY ="AIzaSyAfzk8WDc9Riy5JmmHspZ09WVCeiYXxL1Q"
 
 export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            videoList:[]
+        }
+    }
+    onTermSubmit = term =>{
+        youtube.get("/search",{
+            params:{
+                q: term,
+                part:"snippet",
+                type:'video',
+                maxResults:5,
+                key:KEY
+            }
+        }).then(res=>{
+            this.setState({
+                videoList:res.data.items
+            })
+        })
+        .catch(err=>{console.log(err)})
+    }
     render() {
+
         return (
-            <div>
-                <SearchBar/>
+            <div className="ui container">
+                <SearchBar onFormSubmited={this.onTermSubmit}/>
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+    state = {searchTerm:''}
+    onInputChange = e =>{
+        this.setState({
+            searchTerm:e.target.value
+        })
+    }
+    onFormSubmit = e =>{
+        e.preventDefault()
+       this.props.onFormSubmited(this.state.searchTerm)
+    }
     render() {
         return (
-            <div>
-                Search bar is here
+            <div className="search-bar ui segment">
+               <form className="ui form" onSubmit={this.onFormSubmit}>
+                <div className="field">
+                <label>Search Youtube video</label>
+                <input type="text" value={this.state.searchTerm} onChange={this.onInputChange}/>
+                </div>
+               </form>
             </div>
         )
     }
